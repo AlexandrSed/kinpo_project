005f8b8ee89d9055fe650b90159ef5fa61c45b89
SedovAlexandr <sedovaleksandr808@gmail.com>
2022-06-11 19:30:46 +0300

    добавлена реализация функции determineSidesOfParallelogram

--- main.cpp
+++ main.cpp
@@ -22,6 +22,54 @@ int main(int argc, char *argv[])
 
 void determineSidesOfParallelogram(parallelogram &ABCD)
 {
+    segment AB(ABCD.A, ABCD.B), CD(ABCD.C, ABCD.D);
+    segment AC(ABCD.A, ABCD.C), DB(ABCD.D, ABCD.B);
+    segment AD(ABCD.A, ABCD.D), BC(ABCD.B, ABCD.C);
+
+    // если прямая АВ параллельна прямой CD…
+    if(AB.determineMutualArrangementOfLines(CD) == parallel)
+    {
+        // если AC параллельна BD…
+        if(AC.determineMutualArrangementOfLines(DB) == parallel)
+        {
+            // вернуть: стороны параллелограмма AB, CD, AC, BD
+            ABCD.side1 = AB;
+            ABCD.side2 = CD;
+            ABCD.side3 = AC;
+            ABCD.side4 = DB;
+        }
+        else if(AD.determineMutualArrangementOfLines(BC) == parallel) // иначе, если AD параллельна BC…
+        {
+            // вернуть: стороны параллелограмма AB, CD, AD, BC
+            ABCD.side1 = AB;
+            ABCD.side3 = CD;
+            ABCD.side4 = AD;
+            ABCD.side2 = BC;
+        }
+        else // иначе…
+        {
+            // выдать ошибку: указанный четырёхугольник не является параллелограммом
+        }
+    }
+    else if(AC.determineMutualArrangementOfLines(DB) == parallel) // иначе, если AC параллельна BD…
+    {
+        if(AD.determineMutualArrangementOfLines(BC) == parallel) // если AD параллельна BC…
+        {
+            // вернуть: стороны параллелограмма AC, BD, AD, BC
+            ABCD.side4 = AC;
+            ABCD.side2 = DB;
+            ABCD.side1 = AD;
+            ABCD.side3 = BC;
+        }
+        else // иначе…
+        {
+            // выдать ошибку: указанный четырёхугольник не является параллелограммом
+        }
+    }
+    else // иначе…
+    {
+        // выдать ошибку: указанный четырёхугольник не является параллелограммом
+    }
 
 }
 
